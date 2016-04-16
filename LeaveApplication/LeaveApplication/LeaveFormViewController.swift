9f30992fa53c8d1f25bc48c1718073b483831a56
Ruaan Erasmus <ruaan@tangentsolutions.co.za>
2016-04-16 15:03:48 +0200

    last designs

---
+++ LeaveApplication/LeaveApplication/LeaveFormViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LeaveFormViewController.swift
+//  LeaveApplication
+//
+//  Created by Ruaan Erasmus on 2016/04/16.
+//  Copyright © 2016 Tangent Solutions. All rights reserved.
+//
+
+import UIKit
+
+class LeaveFormViewController: UIViewController {
+
+    @IBAction func CloseButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
